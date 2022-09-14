33d7fdd7cb69882dcf27fb658d3996332c7634f7
ruslan <safiulinr.r@yandex.ru>
2022-09-14 18:50:23 +0300

    создал программу, которая из имеющегося массива строк формирует новый массив строк длина которых <= 3

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+var arr = new string[] { ":)", "Hello", "Kazan", "Rus", "Tommy", "12" };
+ 
+string[] group = Array.FindAll(arr, str => str.Length <= 3);
+foreach (var str in group) Console.Write($"{str} ");
+Console.WriteLine();
